2e4709cf7b38517e75cb5c526648a24a2b3f8d11
MOreshhhhh <156440721+MOreshhhhh@users.noreply.github.com>
2025-12-25 19:33:17 +0530

    Add login pages

---
+++ login/script.js
@@ -0,0 +1,58 @@
+(function () {
+    
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "api key here",
+  authDomain: "domain here.firebaseapp.com",
+  databaseURL: "database url here",
+  projectId: "project id here",
+  storageBucket: "subject here.appspot.com",
+  messagingSenderId: "id here",
+  appId: "api id here"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    //Get Elements
+    const txtEmail = document.getElementById("txtEmail");
+    const txtPassword = document.getElementById("txtPassword");
+    const btnLogin = document.getElementById("btnLogin");
+    const btnSignup = document.getElementById("btnSignup");
+
+    //Add Login Event
+    btnLogin.addEventListener('click', e => {
+        const email = txtEmail.value;
+        const password = txtPassword.value;
+
+        const auth = firebase.auth();
+
+        //sign in with firebase auth
+        auth.signInWithEmailAndPassword(email, password).then(user =>{
+            alert("Login Successful :)");
+	    window.location.href = "../LetItBe-main/index.html";
+        }).catch(err => {
+            alert(err.message);
+        });
+        
+    });
+
+    //Add Signup Event
+    btnSignup.addEventListener('click', e => {
+
+        //get email and password
+        const email = txtEmail.value;
+        const password = txtPassword.value;
+
+        const auth = firebase.auth();
+
+        //sign in with firebase auth
+        const promise = auth.createUserWithEmailAndPassword(email, password).then(user => {
+            alert("Signup Successful :)")
+        }).catch(err => {
+            alert(err.message);
+        });
+
+    });
+
+
+}());
